66776fa0dbc73022c998f270412f547d42a60cc0
mjn954 <mjn265813@gmail.com>
2025-04-01 01:31:47 +0330

    a

--- laravel/resources/views/master/home.blade.php
+++ laravel/resources/views/master/home.blade.php
@@ -86,13 +86,11 @@
                     </div>
                 </div>
             </div>
-
+            {{-- newsgroup --}}
          @include('layout.newsgroup')
 
     <!-- Footer Section -->
-    <footer class="footer">
-        <p>تمامی حقوق محفوظ است &copy; 2025 | طراحی شده با ❤️ توسط تیم خبرنامه لوکس</p>
-    </footer>
+   @include('layout.footer')
 
     <script src="js/pageone.js"></script>
 </body>
